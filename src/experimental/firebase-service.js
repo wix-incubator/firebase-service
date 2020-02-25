f9c564774c888e297b7411d75a9cff65b0dfcddc
Adir Amsalem <adira@wix.com>
2020-02-25 17:02:05 +0200

    fix build

--- src/experimental/firebase-service.js
+++ src/experimental/firebase-service.js
@@ -5,7 +5,7 @@ class FirebaseService {
   atomicServerTime = false;
 
   constructor(name, {atomicServerTime = false} = {}) {
-    uuid = require('uuid/4');
+    uuid = require('uuid/v4');
     name = name || uuid();
     firebase = require('firebase/app');
     require('firebase/database');
